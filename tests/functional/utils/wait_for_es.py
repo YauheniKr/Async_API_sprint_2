fd293a6451ddfddd053a86f728b6717a90f12ac4
YauheniKr <smilodon2@yandex.ru>
2021-12-04 13:06:34 +0300

    добавил логирование удалил принт

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,8 +1,12 @@
+import logging
 from time import sleep
 
 import requests
 
-URL = 'http://192.168.88.131:9200/'
+URL = 'http://elasticsearch:9200/'
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def make_request():
@@ -14,11 +18,11 @@ def check_es_status():
     while True:
         try:
             make_request()
-            print("Elasticsearch Running")
+            logger.info("Elasticsearch Running")
             exit(0)
         except Exception as e:
-            print(e.args[0].reason.args[0].split(':')[1].strip())
-            print("Elasticsearch is not available. Sleep for 10 sec")
+            logger.error(e.args[0].reason.args[0].split(':')[1].strip())
+            logger.error("Elasticsearch is not available. Sleep for 10 sec")
             sleep(10)
         else:
             exit(1)
