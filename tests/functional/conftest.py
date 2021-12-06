9347974f6779a4954ac889371ccab6ed6e195036
YauheniKr <smilodon2@yandex.ru>
2021-12-07 02:11:22 +0300

    добавил необходимые импорты модулей  добавил фикстуру асинхронной петли  вынес загрузку тетсовых данных в отдельную фикстуру  переделал параметры для доступа к сервисам

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,15 +1,45 @@
+import asyncio
+import logging
+
 import aiohttp
 import pytest
 from elasticsearch import AsyncElasticsearch
 
-from tests.functional.settings import SERVICE_URL, ELASTIC_HOST
+from tests.functional.utils.etl_loader import ETLLoader
+from tests.functional.settings import test_settings, SCHEMA_DIR, TESTDATA_DIR
 from tests.functional.utils.models import HTTPResponse
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@pytest.yield_fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(scope='session')
-async def es_client():
-    client = AsyncElasticsearch(hosts=ELASTIC_HOST)
+def film_data_prepare():
+    film_data = {
+        'index': 'movies',
+        'index_file': SCHEMA_DIR.joinpath('elastic_movies_schema.json'),
+        'test_data': TESTDATA_DIR.joinpath('movies_test_data.json'),
+    }
+    return film_data
+
+@pytest.fixture(scope='session')
+async def es_client(film_data_prepare):
+    elastic_host = f'{test_settings.ELASTIC_HOST}:{test_settings.ELASTIC_PORT}'
+    client = AsyncElasticsearch(hosts=elastic_host)
+    etl_loader = ETLLoader(film_data_prepare['index'], client)
+    await etl_loader.index_creation(film_data_prepare['index_file'])
+    await etl_loader.load_to_es(film_data_prepare['test_data'])
     yield client
+    await etl_loader.destroy_es_index()
+    logger.info(client.info)
     await client.close()
 
 
@@ -24,9 +54,8 @@ async def session():
 def make_get_request(session):
     async def inner(method: str = None, params: dict = None) -> HTTPResponse:
         params = params or {}
-        # url = SERVICE_URL + '/api/v1' + method  # в боевых системах старайтесь так не делать!
-        url = f'{SERVICE_URL}/api/v1/film/{method}'
+        url = f'http://{test_settings.SERVICE_HOST}:{test_settings.SERVICE_PORT}/api/v1/film/{method}'
         async with session.get(url, params=params) as response:
-            return HTTPResponse(body=await response.json(), headers=response.headers, status=response.status,)
+            return HTTPResponse(body=await response.json(), headers=response.headers, status=response.status, )
 
     return inner
