f2c6709e3febfa5eb412f5b94e31bb6a18e34c75
YauheniKr <smilodon2@yandex.ru>
2021-12-07 02:15:08 +0300

    убрал лишнию логику не относящуюся к тестам

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -1,18 +1,20 @@
 import pytest
 
+from tests.functional.conftest import make_get_request
 from tests.functional.settings import TESTDATA_DIR, SCHEMA_DIR
 from tests.functional.utils.etl_loader import ETLLoader
 
 
 class TestsFilmsApi:
 
+
     @pytest.mark.asyncio
     async def test_film_detailed(self, es_client, make_get_request): #Todo: вынести в Abstract class
-        etl_loader = ETLLoader('movies', es_client)
-        index_file = SCHEMA_DIR.joinpath('elastic_movies_schema.json')
-        await etl_loader.index_creation(index_file)
-        test_data_filepath = TESTDATA_DIR.joinpath('movies_test_data.json')
-        await etl_loader.load_to_es(test_data_filepath)
+        #etl_loader = ETLLoader(self.INDEX, es_client)
+        #index_file = SCHEMA_DIR.joinpath('elastic_movies_schema.json')
+
+        #test_data_filepath = TESTDATA_DIR.joinpath('movies_test_data.json')
+        #await etl_loader.load_to_es(self.TEST_DATA)
 
         response = await make_get_request('00af52ec-9345-4d66-adbe-50eb917f463a')
 
@@ -22,3 +24,6 @@ class TestsFilmsApi:
         # assert len(response.body) == 1
 
         # assert response.body == expected
+
+    async def test(self):
+        pass
