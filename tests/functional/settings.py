e6cc54c2f1dd16a86f693b0f183ed20374643d6a
YauheniKr <smilodon2@yandex.ru>
2021-12-07 02:01:13 +0300

    перенес часть настроек в класс pydantic

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -1,3 +1,4 @@
+from pydantic import BaseSettings
 from pydantic.parse import Path
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -5,5 +6,22 @@ TESTDATA_DIR = Path(*[BASE_DIR / 'testdata' / 'data'])
 SCHEMA_DIR = Path(*[BASE_DIR / 'testdata' / 'schema'])
 UTILS_DIR = [BASE_DIR / 'utils']
 
-SERVICE_URL = 'http://async_api:8000'
-ELASTIC_HOST = 'http://elasticsearch:9200'
+
+class TestSettings(BaseSettings):
+    # Настройки Redis
+    REDIS_HOST: str = 'redis'
+    REDIS_PORT: int = 6379
+
+    # Настройки Elasticsearch
+    ELASTIC_HOST: str = 'elasticsearch'
+    ELASTIC_PORT: int = 9200
+
+    # Настройки сервиса
+    SERVICE_HOST: str = 'async_api'
+    SERVICE_PORT: int = 8000
+
+    class Config:
+        case_sensitive = True
+
+
+test_settings = TestSettings()
