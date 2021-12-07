fec61a98fc2919f768dbf6ecace97d62f8a261f9
yauheni <smilodon2@yandex.ru>
2021-12-07 18:14:06 +0300

    Удалил неиспользуемые импорты Переделал запуск теста Добавил тест списка фильмов

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -1,29 +1,21 @@
 import pytest
 
-from tests.functional.conftest import make_get_request
-from tests.functional.settings import TESTDATA_DIR, SCHEMA_DIR
-from tests.functional.utils.etl_loader import ETLLoader
+#from tests.functional.conftest import make_get_request, film_data_prepare
+#from tests.functional.settings import TESTDATA_DIR, SCHEMA_DIR
+#from tests.functional.utils.etl_loader import ETLLoader
 
 
 class TestsFilmsApi:
 
-
     @pytest.mark.asyncio
-    async def test_film_detailed(self, es_client, make_get_request): #Todo: вынести в Abstract class
-        #etl_loader = ETLLoader(self.INDEX, es_client)
-        #index_file = SCHEMA_DIR.joinpath('elastic_movies_schema.json')
-
-        #test_data_filepath = TESTDATA_DIR.joinpath('movies_test_data.json')
-        #await etl_loader.load_to_es(self.TEST_DATA)
-
+    async def test_film_detailed(self, film_data_prepare, make_get_request): #Todo: вынести в Abstract class
         response = await make_get_request('00af52ec-9345-4d66-adbe-50eb917f463a')
-
-        # Проверка результата
         assert response.status == 200
         assert response.body['uuid'] == '00af52ec-9345-4d66-adbe-50eb917f463a'
-        # assert len(response.body) == 1
 
-        # assert response.body == expected
+    @pytest.mark.asyncio
+    async def test_film_list(self, film_data_prepare, make_get_request):
+        response = await make_get_request('?sort=-imdb_rating')
+        assert response.status == 200
 
-    async def test(self):
-        pass
+        assert len(response.body) == 4
