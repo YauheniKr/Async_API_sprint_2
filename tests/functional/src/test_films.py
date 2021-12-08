6f3ae8ee753098ad1fc20e76b8116871a2476116
YauheniKr <smilodon2@yandex.ru>
2021-12-08 03:55:38 +0300

    переопредлил используемые фикстуры, добавил больший объем тестов

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -1,21 +1,19 @@
 import pytest
 
-#from tests.functional.conftest import make_get_request, film_data_prepare
-#from tests.functional.settings import TESTDATA_DIR, SCHEMA_DIR
-#from tests.functional.utils.etl_loader import ETLLoader
+from tests.functional.utils.etl_loader import Loader
 
 
 class TestsFilmsApi:
 
     @pytest.mark.asyncio
-    async def test_film_detailed(self, film_data_prepare, make_get_request): #Todo: вынести в Abstract class
+    async def test_film_detailed(self, es_client, film_data_prepare, make_get_request):
         response = await make_get_request('00af52ec-9345-4d66-adbe-50eb917f463a')
         assert response.status == 200
         assert response.body['uuid'] == '00af52ec-9345-4d66-adbe-50eb917f463a'
 
     @pytest.mark.asyncio
-    async def test_film_list(self, film_data_prepare, make_get_request):
+    async def test_film_list(self, es_client, film_data_prepare, make_get_request):
         response = await make_get_request('?sort=-imdb_rating')
         assert response.status == 200
+        assert len(response.body) == 50
 
-        assert len(response.body) == 4
