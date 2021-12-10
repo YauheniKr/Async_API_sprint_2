df26f92dae6d54d90f89b43f3e22cc4533e1ddd3
YauheniKr <smilodon2@yandex.ru>
2021-12-10 22:38:23 +0300

    добавил параметры для тестов добавил тест для проверки статуса отстутсвия данных в индексе жанры

--- tests/functional/src/test_genre.py
+++ tests/functional/src/test_genre.py
@@ -1,17 +1,36 @@
 import pytest
 
+from tests.functional.settings import SCHEMA_DIR, TESTDATA_DIR
+
 
 class TestsGenreApi:
+    GENRE_TEST_DATA = {
+        'index': 'genre',
+        'index_file': SCHEMA_DIR.joinpath('elastic_genre_schema.json'),
+        'test_data': TESTDATA_DIR.joinpath('genre_test_data.json'),
+    }
 
     @pytest.mark.asyncio
-    async def test_genre_detailed(self, redis_client, es_client, film_data_prepare, make_get_request):
+    @pytest.mark.parametrize('es_client', [GENRE_TEST_DATA], indirect=True)
+    async def test_genre_detailed(self, es_client, make_get_request):
         response = await make_get_request('genre/6c162475-c7ed-4461-9184-001ef3d9f26e')
         assert response.status == 200
         assert response.body['uuid'] == '6c162475-c7ed-4461-9184-001ef3d9f26e'
         assert response.body['name'] == 'Sci-Fi'
 
     @pytest.mark.asyncio
-    async def test_genre_list(self, redis_client, es_client, film_data_prepare, make_get_request):
+    @pytest.mark.parametrize('es_client', [GENRE_TEST_DATA], indirect=True)
+    async def test_genre_list(self, es_client, make_get_request):
         response = await make_get_request('genre')
         assert response.status == 200
         assert len(response.body) == 26
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('es_client', [GENRE_TEST_DATA], indirect=True)
+    async def test_genre_list_empty(self, redis_client, es_client, make_get_request):
+        await redis_client.flushall()
+        await es_client.indices.delete(self.GENRE_TEST_DATA['index'])
+        response = await make_get_request('genre')
+        assert response.status == 200
+        assert len(response.body) == 0
+        assert response.body == []
