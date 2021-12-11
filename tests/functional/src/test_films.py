334a7bc4935fd8a9cea4372f23ca2042e480e3d5
YauheniKr <smilodon2@yandex.ru>
2021-12-11 17:46:25 +0300

    вынес загрузку данных в отдельную фикстуру удалил параметризацию

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -1,41 +1,30 @@
 import pytest
 
-from tests.functional.settings import SCHEMA_DIR, TESTDATA_DIR
-
 
 class TestsFilmsApi:
-    FILM_TEST_DATA = {
-        'index': 'movies',
-        'index_file': SCHEMA_DIR.joinpath('elastic_movies_schema.json'),
-        'test_data': TESTDATA_DIR.joinpath('movies_test_data.json'),
-    }
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('es_client', [FILM_TEST_DATA], indirect=True)
-    async def test_film_detailed(self, es_client, make_get_request):
+    async def test_film_detailed(self, load_movies_to_es, make_get_request, redis_client):
         response = await make_get_request('film/00af52ec-9345-4d66-adbe-50eb917f463a')
         assert response.status == 200
         assert response.body['uuid'] == '00af52ec-9345-4d66-adbe-50eb917f463a'
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('es_client', [FILM_TEST_DATA], indirect=True)
-    async def test_film_list(self, es_client, make_get_request):
+    async def test_film_list(self, load_movies_to_es, make_get_request):
         response = await make_get_request('film?sort=-imdb_rating')
         assert response.status == 200
         assert response.body == sorted(response.body, key=lambda x: -x['imdb_rating'])
         assert len(response.body) == 50
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('es_client', [FILM_TEST_DATA], indirect=True)
-    async def test_film_list_custom_pagination(self, es_client, make_get_request):
+    async def test_film_list_custom_pagination(self, load_movies_to_es, make_get_request):
         response = await make_get_request('film', params={'sort': '-imdb_rating', 'page[size]': 20, 'page[number]': 1})
         assert response.status == 200
         assert response.body == sorted(response.body, key=lambda x: -x['imdb_rating'])
         assert len(response.body) == 20
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('es_client', [FILM_TEST_DATA], indirect=True)
-    async def test_film_list_filter(self, es_client, make_get_request):
+    async def test_film_list_filter(self, load_movies_to_es, make_get_request):
         response = await make_get_request('film',
                                           params={'sort': '-imdb_rating', 'page[size]': 20, 'page[number]': 1,
                                                   'filter[genre]': '1cacff68-643e-4ddd-8f57-84b62538081a'})
--- tests/functional/src/test_genre.py
+++ tests/functional/src/test_genre.py
@@ -4,32 +4,23 @@ from tests.functional.settings import SCHEMA_DIR, TESTDATA_DIR
 
 
 class TestsGenreApi:
-    GENRE_TEST_DATA = {
-        'index': 'genre',
-        'index_file': SCHEMA_DIR.joinpath('elastic_genre_schema.json'),
-        'test_data': TESTDATA_DIR.joinpath('genre_test_data.json'),
-    }
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('es_client', [GENRE_TEST_DATA], indirect=True)
-    async def test_genre_detailed(self, es_client, make_get_request):
+    async def test_genre_detailed(self, load_genre_to_es, redis_client, make_get_request):
         response = await make_get_request('genre/6c162475-c7ed-4461-9184-001ef3d9f26e')
         assert response.status == 200
         assert response.body['uuid'] == '6c162475-c7ed-4461-9184-001ef3d9f26e'
         assert response.body['name'] == 'Sci-Fi'
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('es_client', [GENRE_TEST_DATA], indirect=True)
-    async def test_genre_list(self, es_client, make_get_request):
+    async def test_genre_list(self, load_genre_to_es, redis_client, make_get_request):
         response = await make_get_request('genre')
         assert response.status == 200
         assert len(response.body) == 26
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('es_client', [GENRE_TEST_DATA], indirect=True)
-    async def test_genre_list_empty(self, redis_client, es_client, make_get_request):
+    async def test_genre_list_empty(self, redis_client, make_get_request):
         await redis_client.flushall()
-        await es_client.indices.delete(self.GENRE_TEST_DATA['index'])
         response = await make_get_request('genre')
         assert response.status == 200
         assert len(response.body) == 0
