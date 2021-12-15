c7a9ce2fd5784bbcd5ebb69c31800c5d09ebb5f6
YauheniKr <smilodon2@yandex.ru>
2021-12-16 01:20:23 +0300

    заменил везде статус 200 на HTTPStatus.OK

--- tests/functional/src/test_genre.py
+++ tests/functional/src/test_genre.py
@@ -1,6 +1,6 @@
-import pytest
+from http import HTTPStatus
 
-from tests.functional.settings import SCHEMA_DIR, TESTDATA_DIR
+import pytest
 
 
 class TestsGenreApi:
@@ -8,20 +8,20 @@ class TestsGenreApi:
     @pytest.mark.asyncio
     async def test_genre_detailed(self, load_genre_to_es, redis_client, make_get_request):
         response = await make_get_request('genre/6c162475-c7ed-4461-9184-001ef3d9f26e')
-        assert response.status == 200
+        assert response.status == HTTPStatus.OK
         assert response.body['uuid'] == '6c162475-c7ed-4461-9184-001ef3d9f26e'
         assert response.body['name'] == 'Sci-Fi'
 
     @pytest.mark.asyncio
     async def test_genre_list(self, load_genre_to_es, redis_client, make_get_request):
         response = await make_get_request('genre')
-        assert response.status == 200
+        assert response.status == HTTPStatus.OK
         assert len(response.body) == 26
 
     @pytest.mark.asyncio
     async def test_genre_list_empty(self, redis_client, make_get_request):
         await redis_client.flushall()
         response = await make_get_request('genre')
-        assert response.status == 200
+        assert response.status == HTTPStatus.OK
         assert len(response.body) == 0
         assert response.body == []
--- tests/functional/src/test_persons.py
+++ tests/functional/src/test_persons.py
@@ -1,6 +1,6 @@
-import pytest
+from http import HTTPStatus
 
-from tests.functional.settings import SCHEMA_DIR, TESTDATA_DIR
+import pytest
 
 
 class TestsPersonApi:
@@ -9,7 +9,7 @@ class TestsPersonApi:
     async def test_person_detailed(self, load_person_to_es, load_movies_to_es,
                                    redis_client, make_get_request):
         response = await make_get_request('person/26e83050-29ef-4163-a99d-b546cac208f8')
-        assert response.status == 200
+        assert response.status == HTTPStatus.OK
         assert response.body == {
             "uuid": "26e83050-29ef-4163-a99d-b546cac208f8",
             "full_name": "Mark Hamill",
@@ -24,7 +24,7 @@ class TestsPersonApi:
     @pytest.mark.asyncio
     async def test_person_films(self, load_person_to_es, load_movies_to_es, redis_client, make_get_request):
         response = await make_get_request('person/26e83050-29ef-4163-a99d-b546cac208f8/film')
-        assert response.status == 200
+        assert response.status == HTTPStatus.OK
         assert response.body == [
             {'id': '025c58cd-1b7e-43be-9ffb-8571a613579b',
              'imdb_rating': 8.3,
