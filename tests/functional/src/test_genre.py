97f32eca838e96b25110a93c629d65fecee31ed0
YauheniKr <smilodon2@yandex.ru>
2021-12-16 23:13:16 +0300

    адаптировал тесты по замечаниям ревьювера

--- tests/functional/src/test_genre.py
+++ tests/functional/src/test_genre.py
@@ -2,21 +2,28 @@ from http import HTTPStatus
 
 import pytest
 
+from tests.functional.conftest import GENRE_TEST_DATA, load_movies_to_es
+
 
 class TestsGenreApi:
 
     @pytest.mark.asyncio
     async def test_genre_detailed(self, load_genre_to_es, redis_client, make_get_request):
-        response = await make_get_request('genre/6c162475-c7ed-4461-9184-001ef3d9f26e')
+        test_data = load_genre_to_es._get_json_data(GENRE_TEST_DATA['test_data'])[0]
+        test_id = test_data['id']
+        response = await make_get_request(f'genre/{test_id}')
         assert response.status == HTTPStatus.OK
-        assert response.body['uuid'] == '6c162475-c7ed-4461-9184-001ef3d9f26e'
-        assert response.body['name'] == 'Sci-Fi'
+        assert response.body['uuid'] == test_data['id']
+        assert response.body['name'] == test_data['name']
 
     @pytest.mark.asyncio
     async def test_genre_list(self, load_genre_to_es, redis_client, make_get_request):
         response = await make_get_request('genre')
+        tests_data = load_genre_to_es._get_json_data(GENRE_TEST_DATA['test_data'])
+        tests_data = [{'uuid': test_data['id'], 'name': test_data['name']} for test_data in tests_data]
         assert response.status == HTTPStatus.OK
         assert len(response.body) == 26
+        assert response.body == tests_data
 
     @pytest.mark.asyncio
     async def test_genre_list_empty(self, redis_client, make_get_request):
