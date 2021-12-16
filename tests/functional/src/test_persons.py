58883b9f48967b45ddcaa6f091a3c8a5e8b762ab
YauheniKr <smilodon2@yandex.ru>
2021-12-17 00:09:16 +0300

    откорректироал тесты по замечаниям ревьювера убрал захардкоженную тестовые данные

--- tests/functional/src/test_persons.py
+++ tests/functional/src/test_persons.py
@@ -2,34 +2,29 @@ from http import HTTPStatus
 
 import pytest
 
+from tests.functional.conftest import PERSON_TEST_DATA, FILM_TEST_DATA, prepare_person_film_data
+
 
 class TestsPersonApi:
 
     @pytest.mark.asyncio
     async def test_person_detailed(self, load_person_to_es, load_movies_to_es,
                                    redis_client, make_get_request):
-        response = await make_get_request('person/26e83050-29ef-4163-a99d-b546cac208f8')
+        test_person_data = load_person_to_es._get_json_data(PERSON_TEST_DATA['test_data'])[0]
+        test_data_id = test_person_data['id']
+        response = await make_get_request(f'person/{test_data_id}')
         assert response.status == HTTPStatus.OK
-        assert response.body == {
-            "uuid": "26e83050-29ef-4163-a99d-b546cac208f8",
-            "full_name": "Mark Hamill",
-            "role": [
-                "actor",
-                "director"
-            ],
-            'film_ids': ['025c58cd-1b7e-43be-9ffb-8571a613579b',
-                         '0312ed51-8833-413f-bff5-0e139c11264a']
-        }
+        assert response.body['uuid'] == test_data_id
+        assert response.body['full_name'] == test_person_data['full_name']
+        assert response.body['role'] == test_person_data['role']
 
     @pytest.mark.asyncio
     async def test_person_films(self, load_person_to_es, load_movies_to_es, redis_client, make_get_request):
-        response = await make_get_request('person/26e83050-29ef-4163-a99d-b546cac208f8/film')
+        test_person_data = load_person_to_es._get_json_data(PERSON_TEST_DATA['test_data'])[0]
+        test_movies_data = load_movies_to_es._get_json_data(FILM_TEST_DATA['test_data'])
+        test_data_id = test_person_data['id']
+        test_data_dict = {'id': test_person_data['id'], 'name': test_person_data['full_name']}
+        test_film_person_data = prepare_person_film_data(test_data_dict, test_movies_data)
+        response = await make_get_request(f'person/{test_data_id}/film')
         assert response.status == HTTPStatus.OK
-        assert response.body == [
-            {'id': '025c58cd-1b7e-43be-9ffb-8571a613579b',
-             'imdb_rating': 8.3,
-             'title': 'Star Wars: Episode VI - Return of the Jedi'},
-            {'id': '0312ed51-8833-413f-bff5-0e139c11264a',
-             'imdb_rating': 8.7,
-             'title': 'Star Wars: Episode V - The Empire Strikes Back'},
-        ]
+        assert response.body == test_film_person_data
